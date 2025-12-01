55dbd80a4d7e90abdedb9a4a8c91d70aa88954d0
Mani5746 <srivastavmanish389@gmail.com>
2025-12-02 00:19:50 +0530

    The Presentation Layer and DTO

---
+++ demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/DemoSpringBootIntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Week2Module.demoSpringBootIntro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootIntroApplication.class, args);
+	}
+
+}
---
+++ demoSpringBootIntro/src/test/java/com/example/Week2Module/demoSpringBootIntro/DemoSpringBootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Week2Module.demoSpringBootIntro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
