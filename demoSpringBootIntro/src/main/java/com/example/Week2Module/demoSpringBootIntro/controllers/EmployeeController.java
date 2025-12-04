884026cd54c6d69d4c61863645562889d6c2fa02
Mani5746 <srivastavmanish389@gmail.com>
2025-12-04 23:16:57 +0530

    Formats to send the Request

--- demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/controllers/EmployeeController.java
+++ demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/controllers/EmployeeController.java
@@ -1,15 +1,44 @@
 package com.example.Week2Module.demoSpringBootIntro.controllers;
 
+import com.example.Week2Module.demoSpringBootIntro.dto.EmployeeDTO;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+import java.time.LocalDate;
 @RestController
 
+@RequestMapping(path = "/employees") // This will create the Default Path  for both Get and Post but It will prefer Get
+// and for POSt,put and also We can use GET via Postman too  We have to use POSTMan
 public class EmployeeController {
-  @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage(){
-      return "Secret message: hjnkmngbd23454";
+
+//  @GetMapping(path = "/getSecretMessage")
+//
+//    public String getMySuperSecretMessage(){
+//      return "Secret message: hjnkmngbd23454";
+//  }
+
+  //@GetMapping("/employees/{employeeID}")
+  @GetMapping(path = "/{employeeID}")
+  public EmployeeDTO getEmployeeID(@PathVariable Long employeeID){
+  return new EmployeeDTO(employeeID,"Manish","abc@gmail.com",25, LocalDate.of(2024,1,12),true);
+}
+
+  @GetMapping(path = "/employees")
+  public String getAllEmployees(@RequestParam(required=false) Integer age,@RequestParam(required=false) String sortBy){
+
+    return "Hi age "+ age + " " +sortBy;
   }
+
+  @PostMapping()  // We have to sent it via Client like Postman
+  public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+
+    inputEmployee.setId(100L);
+    return inputEmployee;
+  }
+
+  @PutMapping()
+  public String updateEmployee(){
+    return "Hello from Put";
+  }
+
 }
--- demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/dto/EmployeeDTO.java
+++ demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/dto/EmployeeDTO.java
@@ -1,4 +1,52 @@
 package com.example.Week2Module.demoSpringBootIntro.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
 public class EmployeeDTO {
+
+    @Setter
+    @Getter
+    private Long id;
+
+    @Setter
+    @Getter
+    private String name;
+
+    @Setter
+    @Getter
+    private String email;
+
+    @Getter
+    @Setter
+    private Integer age;
+
+    @Setter
+    @Getter
+    private LocalDate dateOfJoining;
+
+    private Boolean isActive;
+
+    public EmployeeDTO(){
+
+    }
+
+    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfJoining = dateOfJoining;
+        this.isActive = isActive;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
