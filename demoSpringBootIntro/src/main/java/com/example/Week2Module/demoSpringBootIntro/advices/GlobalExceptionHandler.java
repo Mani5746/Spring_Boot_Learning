c8df9b7a39901c17803bd04f798f325b016dbeb0
Mani5746 <srivastavmanish389@gmail.com>
2025-12-11 23:09:10 +0530

    API Response way

--- demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/advices/GlobalExceptionHandler.java
+++ demoSpringBootIntro/src/main/java/com/example/Week2Module/demoSpringBootIntro/advices/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.Week2Module.demoSpringBootIntro.advices;
 
 import com.example.Week2Module.demoSpringBootIntro.exceptions.ResourceNotFoundException;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,43 +8,62 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception){
-    ApiError apiError =ApiError.builder()
-            .status(HttpStatus.NOT_FOUND)
-            .message(exception.getMessage())
-            .build();
-    return  new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
-    }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleInternalServerError(Exception exception){
-        ApiError apiError =ApiError.builder()
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception) {
+        ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(exception.getMessage())
                 .build();
-        return  new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
+        return buildErrorResponseEntity(apiError);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception) {
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleInputValidationErrors(MethodArgumentNotValidException exception){
-        List<String> errors=exception.
-                getBindingResult()
+    public ResponseEntity<ApiResponse<?>> handleInputValidationErrors(MethodArgumentNotValidException exception) {
+        List<String> errors = exception
+                .getBindingResult()
                 .getAllErrors()
                 .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
 
-        ApiError apiError =ApiError.builder()
+        ApiError apiError = ApiError.builder()
                 .status(HttpStatus.BAD_REQUEST)
-                .message("Input Validation failed")
+                .message("Input validation failed")
                 .subErrors(errors)
                 .build();
-        return  new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
+        return buildErrorResponseEntity(apiError);
     }
+
+    private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
